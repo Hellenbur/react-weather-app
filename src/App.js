91c591640ff155e2376f4d5a3aba30a6ffd9e8d7
Hellenbur <112714242+Hellenbur@users.noreply.github.com>
2023-04-04 19:46:21 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1> Weather App</h1>
+    <footer>
+      This project was coded by Olena Burma and is {" "}
+      <a href="https://github.com/Hellenbur/react-weather-app" target="_blank">open-soursed on GitHub
+      </a>
+      </footer>
+  </div>;
 }
 
  
