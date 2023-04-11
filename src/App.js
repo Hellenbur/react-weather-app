daca1cdae3302c05a01404ed24be0d2f839e05d9
Hellenbur <112714242+Hellenbur@users.noreply.github.com>
2023-04-11 14:09:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <h1> Weather App</h1>
     <footer>
       This project was coded by Olena Burma and is {" "}
-      <a href="https://github.com/Hellenbur/react-weather-app" target="_blank">open-soursed on GitHub
+      <a href="https://github.com/Hellenbur/react-weather-app" target="_blank" rel="noreferrer" >open-soursed on GitHub
       </a>
       </footer>
   </div>;
